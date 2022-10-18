96feb7f17b87b8f589bf6ce98825173b143b0d47
Ssxund3r <https://github.com/Ssxund3r>
2022-10-17 21:26:21 -0300

    First Commmit

---
+++ projeto-caixaki/src/main/java/br/com/framework/hibernate/session/HibernateUtil.java
@@ -0,0 +1,117 @@
+package br.com.framework.hibernate.session;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.SessionFactoryImplementor;
+
+import br.com.framework.implementacao.crud.VariavelConexaoUtil;
+
+/*
+ * Responsável por estabelecer a conexção com hibernate
+ * @author Gabriel Costa*/
+@ApplicationScoped
+public class HibernateUtil implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	/*
+	 * Responsável por ler o arquivo da configuração hibernate.cfg.xml
+	 * @return SessionFactory
+	 * @author Gabriel Costa
+	 */
+	private static SessionFactory buildSessionFactory() {
+		try {
+			if (sessionFactory == null) {
+				sessionFactory = new Configuration().configure().buildSessionFactory();
+			}
+
+			return sessionFactory;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ExceptionInInitializerError("Erro ao Criar Conexão SessionFactory!");
+		}
+	}
+
+	/**
+	 * Retorna o SessionFactory corrente
+	 * 
+	 * @return SessionFactory
+	 * @author Gabriel Costa
+	 */
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	/**
+	 * Retorna a sessão do SessionFactory
+	 * 
+	 * @return Session
+	 * @author Gabriel Costa
+	 */
+	public static Session getCurrentSession() {
+		return getSessionFactory().getCurrentSession();
+	}
+
+	/**
+	 * Abre uma nova sessão no SessionFactory
+	 * 
+	 * @return Session
+	 * @author Gabriel Costa
+	 */
+	public static Session openSession() {
+		if (sessionFactory == null) {
+			buildSessionFactory();
+		}
+		return sessionFactory.openSession();
+	}
+
+	/**
+	 * Obtem a conection do provedor de conexões configurado
+	 * 
+	 * @return Connection SQL
+	 * @Throw SQLException
+	 * @author Gabriel Costa
+	 */
+	public static Connection getConnectionProvider() throws SQLException {
+		return ((SessionFactoryImplementor) sessionFactory).getConnectionProvider().getConnection();
+	}
+
+	/**
+	 * 
+	 * @return Connection no InitialContext java:/comp/env/jdbc/datasource
+	 * @throws Exception
+	 * @author Gabriel Costa
+	 */
+	public static Connection getConnection() throws Exception {
+		InitialContext context = new InitialContext();
+		DataSource ds = (DataSource) context.lookup(JAVA_COMP_ENV_JDBC_DATA_SOURCE);
+
+		return ds.getConnection();
+	}
+	
+	/**
+	 * 
+	 * @return DataSource JNDI Tomcat
+	 * @throws NamingException
+	 * @author Gabriel Costa
+	 */
+	public DataSource getDataSourcerJndi() throws NamingException {
+		InitialContext context = new InitialContext();
+
+		return (DataSource) context.lookup(VariavelConexaoUtil.JAVA_COMP_ENV_JDBC_DATA_SOURCE);
+	}
+
+}
---
+++ projeto-caixaki/src/main/java/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,10 @@
+package br.com.framework.implementacao.crud;
+/*
+ *Nome do Caminho do JNDI datasource TOMCAT
+ *@author Gabriel Costa */
+public class VariavelConexaoUtil {
+	
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = 
+			"java:/comp/env/jdbc/datasource";
+
+}
