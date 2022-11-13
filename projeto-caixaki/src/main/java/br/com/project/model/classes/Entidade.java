92b841a7f58a75e74aebdae5fec57edee9dc069b
Ssxund3r <https://github.com/Ssxund3r>
2022-11-12 23:20:15 -0300

    Configurando HIbernate Envers

---
+++ projeto-caixaki/src/main/java/br/com/project/model/classes/Entidade.java
@@ -0,0 +1,68 @@
+package br.com.project.model.classes;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.envers.Audited;
+
+@Audited
+@Entity
+public class Entidade implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private Long ent_codigo;
+	private String ent_login = null;
+	private String ent_senha;
+	
+	public void setEnt_login(String ent_login) {
+		this.ent_login = ent_login;
+	}
+	
+	public String getEnt_login() {
+		return ent_login;
+	}
+	
+	public void setEnt_senha(String ent_senha) {
+		this.ent_senha = ent_senha;
+	}
+	
+	public String getEnt_senha() {
+		return ent_senha;
+	}
+
+	public Long getEnt_codigo() {
+		return ent_codigo;
+	}
+
+	public void setEnt_codigo(Long ent_codigo) {
+		this.ent_codigo = ent_codigo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ent_codigo == null) ? 0 : ent_codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Entidade other = (Entidade) obj;
+		if (ent_codigo == null) {
+			if (other.ent_codigo != null)
+				return false;
+		} else if (!ent_codigo.equals(other.ent_codigo))
+			return false;
+		return true;
+	}
+}
