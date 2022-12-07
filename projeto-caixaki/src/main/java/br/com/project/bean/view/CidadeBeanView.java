b84fe0dd2ae1d9538a6d16fc19f6400613a517d1
Ssxund3r <https://github.com/Ssxund3r>
2022-12-07 00:04:51 -0300

    Criando tela de Cidades - Parte IV

--- projeto-caixaki/src/main/java/br/com/project/bean/view/CidadeBeanView.java
+++ projeto-caixaki/src/main/java/br/com/project/bean/view/CidadeBeanView.java
@@ -1,5 +1,8 @@
 package br.com.project.bean.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 
@@ -16,13 +19,26 @@ import br.com.project.model.classes.Cidade;
 public class CidadeBeanView extends BeanManagedViewAbstract {
 	private static final long serialVersionUID = 1L;
 
+	private String url = "/cadastro/cad_cidade.jsf?faces-redirect=true";
 	private Cidade objetoSelecionado = new Cidade();
 
-	private String url = "/cadastro/cad_cidade.jsf?faces-redirect=true";
+	private List<Cidade> list = new ArrayList<Cidade>();
 
 	@Resource
 	private CidadeController cidadeController;
-
+	
+	@Override
+	public String editar() throws Exception {
+		
+		return url;
+	}
+	
+	@Override
+	public void excluir() throws Exception {
+		cidadeController.delete(objetoSelecionado);
+		novo();
+	}
+	
 	@Override
 	public String save() throws Exception {
 		objetoSelecionado = cidadeController.merge(objetoSelecionado);
@@ -44,8 +60,9 @@ public class CidadeBeanView extends BeanManagedViewAbstract {
 		return objetoSelecionado;
 	}
 
-	/*
-	 * public String getUrl() { return url; }
-	 */
+	public List<Cidade> getList() throws Exception {
+		list = cidadeController.findList(Cidade.class);
+		return list;
+	}
 
 }
